1f7c2b39ac86348336abe293b99ec22c2db54f8b
Daniel Gallagher <daniel-gallagher@outlook.com>
2021-04-01 18:37:16 +0100

    new updates

--- website/run_website.py
+++ website/run_website.py
@@ -1,1 +1,18 @@
+"""
+Run Flask website
+"""
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("home.html")
+
+@app.route("/login/")
+def login():
+    return "Login page"
+
+if __name__ == '__main__':
+    app.run()
