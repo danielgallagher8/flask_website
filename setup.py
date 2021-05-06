5d92d1b8bc3db5ae0a4bf6def0292266c06f5822
danielgallagher8 <51876055+danielgallagher8@users.noreply.github.com>
2021-05-06 22:05:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-#import website
+import website
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
@@ -9,7 +9,7 @@ requirements = ['flask']
 
 setup(
       name="website",
-      version="0.0.3",
+      version=website.__version__,
       author="Daniel Gallagher",
       author_email="daniel-gallagher@outlook.com",
       description="Website",
@@ -17,9 +17,7 @@ setup(
       long_description_content_type="text/markdown",
       url="https://github.com/danielgallagher8/website.git",
       packages=find_packages(),
-      #package_data={"website/static":["*.html", "*.css"], "website/templates":["*.html", "*.css"]},
       include_package_data=True,
-      #packages=['website'],
       zip_safe=False,
       install_requires=requirements,
       classifiers=[
